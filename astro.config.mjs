5fc35af3132cdc5bb6653eff25f17bf79f62dcc5
“GodwinEbikwo” <godwin.a.ebikwo@gmail.com>
2022-11-07 15:38:16 +0000

    meta tags added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
-import storyblok from "@storyblok/astro";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    storyblok({
-      accessToken: "<your-access-token>",
     }),
   ],
 });
