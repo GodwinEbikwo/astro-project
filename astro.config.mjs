8e2df45df02a311b6ca123c3c9397421249ede59
“GodwinEbikwo” <godwin.a.ebikwo@gmail.com>
2022-11-07 07:34:52 +0000

    trying

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
+import storyblok from "@storyblok/astro";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    storyblok({
+      accessToken: "<your-access-token>",
     }),
   ],
 });
